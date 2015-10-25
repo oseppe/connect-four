5207d1d54d3c910af8a992ac3c4edd6b90011e34
jay montalbo <sepmontalbo@gmail.com>
2015-10-25 23:24:47 +0800

    Add gameboard Add drop token functionality

--- app/assets/javascripts/game.coffee
+++ app/assets/javascripts/game.coffee
@@ -3,28 +3,67 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $ ->
+	checker = 'aa'
+	playerOneColor = 'slot-player-one'
+	playerTwoColor = 'slot-player-two'
+	gameboard = new Gameboard(checker, playerOneColor, playerTwoColor, 6, 7)
+
 	# When user hovers over column, change background colour
 	$('.container-slot').mouseover ->
 		# get column number from id
-		col = parseId($(this).attr('id'))[0]
+		col = parseId($(this).attr('id'))['id']
+
+		colSelector = '.col-' + col
 
 		# highlight column
-		colSelector = '.col-' + col
 		$(colSelector).addClass('container-slot-focus');
 		return
 
+	# When user leaves column, return original background colour
 	$('.container-slot').mouseleave ->
 		# get column number from id
-		col = parseId($(this).attr('id'))[0]
+		col = parseId($(this).attr('id'))['id']
+
+		colSelector = '.col-' + col
 
 		# remove highlight of column
-		colSelector = '.col-' + col
 		$(colSelector).removeClass('container-slot-focus');
 		return
 
+	# Drop a token when user clicks column
+	$('.container-slot').click ->
+		# get column number from id
+		col = parseId($(this).attr('id'))['id']
+		# process user's turn
+		turnData = gameboard.dropChip(col)
 
+		if turnData['place']['row'] != -1
+			
+			slotId = getSlotId(turnData['place'])
+
+			# change slot color
+			$(slotId).removeClass('slot-empty')
+			$(slotId).addClass(turnData["chipColor"])
+			# TODO: check if win
+
+		return
+
+# 
+# Use slot coordinate to construct slot id
+#
+# @param string
+# @return string
+getSlotId = (slotCoord) ->
+	slot = '#slot-' + slotCoord['row'] + '-' + slotCoord['col']
+
+	return slot
+
+# 
+# Extract id from string
+#
+# @return object hash
 parseId = (idStr) ->
 	parsedIdStr = idStr.split('-')
 	id = parsedIdStr.pop()
 
-	return [id ,parsedIdStr]
+	return { id:id, parsedIdStr:parsedIdStr }
---
+++ app/assets/javascripts/gameboard.js
@@ -0,0 +1,112 @@
+var Gameboard = function (checker, playerOne, playerTwo, height, width) {
+	this.board = createTwoDArray(height, width),
+	this.columns = width,
+	this.rows = height,
+	this.checker = checker,
+	this.playerOne = playerOne,
+	this.playerTwo = playerTwo,
+	this.turn = 1,
+	
+	/**
+     * Check if someone won
+     *
+     * @param none
+     * @return boolean
+     */
+	this.check = function() {
+		// return this.checker.check(this.board);
+		return false;
+	},
+
+	/**
+     * Restart game
+     *
+     * @param none
+     * @return none
+     */
+    this.restart = function() {
+    	this.board = createTwoDArray(rows, columns);
+		this.turn = 1;
+    }
+	/**
+     * Process user's turn
+     *
+     * @param int
+     * @return object hash
+     */
+	this.dropChip = function(col) {
+		
+		var chipColor = '';
+		var place = {col: col, row: 0};
+		var row = this.updateBoard(col, this.turn);
+		var win = false;
+
+		place['row'] = row;
+
+		// don't change game data if column already filled
+		if (row != -1) {
+
+			switch(this.turn) {
+				case 1:
+					chipColor = this.playerOne;
+					this.turn = 2;
+					break;
+				case 2:
+					chipColor = this.playerTwo;
+					this.turn = 1;
+					break;
+			}
+
+			win = this.check();
+		};
+			
+		return { place: place, chipColor: chipColor, win: win };
+	}
+	// Start game
+	this.startGame = function() {
+
+	},
+
+	/**
+     * Update board to reflect dropped chip
+     *
+     * @param int
+     * @param int
+     * @return int
+     */
+    this.updateBoard = function(col, player) {
+
+    	// get index of last 0 in array
+    	var row = this.board[col].lastIndexOf(0);
+
+    	// dont update board if column already filled
+    	if (row != -1) {
+    		this.board[col][row] = player;
+    	};
+
+    	return row
+    }
+};
+
+/**
+ * Creates an empty two dimensional array
+ *
+ * @param int
+ * @param int
+ * @return array
+ */
+function createTwoDArray(height, width) {
+	var array = new Array(width);
+
+	for (var i = 0 ; i < array.length; i++) {
+		
+		array[i] = new Array(height);
+
+		// fill newly created array with 0
+		for (var j = 0 ; j < height; j++) {
+			array[i][j] = 0;
+		};
+	};
+
+	return array;
+}
